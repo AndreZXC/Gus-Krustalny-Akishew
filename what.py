8908343a335324cc657b1d4b6265e0be2893bbec
Andre <ender07.70redne@gmail.com>
2023-11-24 22:27:48 +0300

    third

--- what.py
+++ what.py
@@ -1,15 +1,15 @@
 import sys
 from PyQt5.QtWidgets import QWidget, QApplication
-from PyQt5 import uic
+from Ui import Ui_Form
 from PyQt5.QtGui import QPainter, QColor
 from random import randint
 
 
-class WhatWindow(QWidget):
+class WhatWindow(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
         self.XXX = False
-        uic.loadUi('Ui.ui', self)
+        self.setupUi(self)
         self.spawnbtn.clicked.connect(self.spawn)
 
     def spawn(self):
@@ -20,7 +20,7 @@ class WhatWindow(QWidget):
         if self.XXX:
             qp = QPainter()
             qp.begin(self)
-            qp.setBrush(QColor(255, 255, 0))
+            qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
             razm = randint(10, 200)
             qp.drawEllipse(randint(0, self.width() - razm), randint(0, self.height() - razm), razm, razm)
             qp.end()
